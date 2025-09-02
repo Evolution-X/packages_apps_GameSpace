9bff284acd405adcb17bc6e1d0b015be06c70631
Pranav Vashi <neobuddy89@gmail.com>
2025-09-02 19:19:42 +0530

    GameSpace: Setup material expressive design for preferences
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2021 Chaldeaprjkt
- * Copyright (C) 2022-2024 crDroid Android Project
+ * Copyright (C) 2022-2025 crDroid Android Project
  *               2023-2024 the risingOS Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,13 +23,16 @@ import android.os.SystemProperties
 import android.view.View
 import androidx.preference.Preference
 import androidx.preference.PreferenceCategory
-import androidx.preference.PreferenceFragmentCompat
+
+import com.android.settingslib.widget.SettingsBasePreferenceFragment
+
 import dagger.hilt.android.AndroidEntryPoint
+
 import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.preferences.QuickStartAppPreference
 import io.chaldeaprjkt.gamespace.preferences.QuickStartAppPreferenceDialogFragment
 
-@AndroidEntryPoint(PreferenceFragmentCompat::class)
+@AndroidEntryPoint(SettingsBasePreferenceFragment::class)
 class SettingsFragment : Hilt_SettingsFragment() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
